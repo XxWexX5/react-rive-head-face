f7238973dd6b701d8ef76ed4d38a47e4d32c067e
Wevison Ramalho Silva <wevison.silva@altoqi.com.br>
2025-05-09 18:00:44 -0300

    [ FEATURE ] Adding animation head face.

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useRive } from "@rive-app/react-canvas";
 
-function App() {
-  const [count, setCount] = useState(0)
+const STATE_MACHINE_NAME = "State Machine 1";
+
+export function App() {
+  const { RiveComponent } = useRive({
+    src: "src/assets/character_man.riv",
+    stateMachines: STATE_MACHINE_NAME,
+    autoplay: true,
+  });
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="w-screen h-screen flex justify-center items-center bg-[#ADD4DE]">
+      <div className="w-full h-full relative z-50">
+        <RiveComponent />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
 
-export default App
+      <div className="absolute w-full inset-0 bg-[radial-gradient(circle,white_0%,transparent_45%)] pointer-events-none" />
+    </div>
+  );
+}
